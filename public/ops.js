5e1313aae80185c979e4a603f8d5a23acaca3d70
ilhomelv <ilhomelv@gmail.com>
2018-02-24 01:24:59 +0500

    changed some codes on private messaging functions on both client and server side files

--- public/ops.js
+++ public/ops.js
@@ -14,7 +14,8 @@ var socket = io.connect(),
     msg = document.getElementById('messageTextArea'),
     chatBtn = document.getElementById('chatButton');
 
-//add event listener on enter button
+
+//Enter to Chat
 enterBtn.addEventListener('click', function(e){
     e.preventDefault();
     socket.emit('new user', userNameInput.value, function(data){
@@ -26,37 +27,86 @@ enterBtn.addEventListener('click', function(e){
         }
     });
 });
+//display usernames on chat inside div displayUsers
+// socket.on('usernames', function(data){
+//     displayUsers.innerHTML = '';
+//     for(var i=0; i<data.length; i++){
+//       displayUsers.innerHTML += `${`<div id="nameOnDisplay-${data[i]}">${data[i]}</div>`}`;    // '<div id="nameOnDisplay-data[i]">' + data[i] + '</div>';
+//       //var htmlStr = `${`<h2>${nameX}</h2>`}`;		//using Template Literal concept
+//   }
+// });
 
-//add event listener on send message button
-chatBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    socket.emit('send message',  msg.value);
-    msg.value = ''; //clear msg value
+
+//choose a person to chat with
+var div_ele=displayUsers.childNodes;
+socket.on('usernames', function(data){
+    displayUsers.innerHTML = '';
+    var htmlStr = '';
+    for(var i=0; i<data.length; i++){
+      htmlStr = '<p>' + data[i]+'</p>';
+      var innerDisplayUsers = document.createElement('div');
+      innerDisplayUsers.id = data[i];// + '-innerDisplayUser';
+      innerDisplayUsers.innerHTML = htmlStr;
+      displayUsers.appendChild(innerDisplayUsers);
+    }
+    for (var i=0; i<div_ele.length; i++) {
+         //console.log(div_ele[i]);
+         div_ele[i].addEventListener('click', chooseUserToChatWith(div_ele[i]));
+    }
 });
+function chooseUserToChatWith(data){
+    return function(){
+      console.log(data);       console.log(data.id);
+      chatDivContainer.style.display = "block";
+      sendMessageToUser(data.id);
+    };
+}
+
+
+//send message to chosen client
+function sendMessageToUser(client) {
+  console.log(client);
+  chatBtn.addEventListener('click', function(e) {  e.preventDefault();
+    socket.emit('send message',  {  'client': client, 'msg': msg.value} );
+    msg.value = '';
+  });
+}
+
 
 //add event listener on msg typing
 msg.addEventListener('keypress', function(){
     socket.emit('typing message', userNameInput.value);
 });
 
-
-
-//Listen on events
-socket.on('usernames', function(data){
-    displayUsers.innerHTML = '';
-    for(var i=0; i<data.length; i++){
-      displayUsers.innerHTML += '<p>' + data[i] + '</p>';
-  }
-});
-
-
+//message output
 socket.on('new message', function(data){
+  console.log("From: " +data.sender);
+  console.log("To: " +data.client);
+  console.log("Message: " +data.message);
+
+  chatDivContainer.style.display = "block";
     typingOutput.innerHTML="";
-    chatOutput.innerHTML += '<p><b>' + data.name +'</b>: '+ data.message + '</p>';
+    var htmlStr = '';
+    htmlStr += '<p><b>' + data.sender +'</b>: '+ data.message + '</p>';
+    var innerChatOutput = document.createElement('div');
+    innerChatOutput.id = data.client;
+    innerChatOutput.innerHTML = htmlStr;
+    chatOutput.appendChild(innerChatOutput);
+
     //keep scroll on bottom
     var xH = chatMessagesContainer.scrollHeight;
     chatMessagesContainer.scrollTo(0, xH);
 });
+
+//Listen on events
+// socket.on('new message', function(data){
+//   chatDivContainer.style.display = "block";
+//     typingOutput.innerHTML="";
+//     chatOutput.innerHTML += '<p><b>' + data.sender +'</b>: '+ data.message + '</p>';
+//     //keep scroll on bottom
+//     var xH = chatMessagesContainer.scrollHeight;
+//     chatMessagesContainer.scrollTo(0, xH);
+// });
 
 socket.on('typing message', function(data){
     console.log(data + '  typing message')
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 //Server setup
 var express = require('express'),
-    app = express(),
+    app = express(),    //simply one line var app = require('express')();
     server = require('http').createServer(app),
     usersNames = {},
     io = require('socket.io').listen(server);
@@ -16,17 +16,17 @@ server.listen(3000, function(){ console.log('listening port 3000')});
 // });
 app.use(express.static('public'));
 
-//listen on events client side
-io.sockets.on('connection', function(socket) {
+//listen on events coming from client side
+io.on('connection', function(socket) {
     //user enters onto chat
     socket.on('new user', function(data, callback){
         if(data in usersNames) { callback(false); }
-        //if(usersNames.indexOf(data) != -1) { callback(false); }
         else {
           callback(true);
-          socket.userName = data;
+          	// we store the username in the socket session for this client
+          socket.userName = data
+          // add the client's username to the global list
           usersNames[socket.userName] = socket;  //userName is key : socket
-          //usersNames.push(socket.userName);
           updateUsersNames();
         }
     });
@@ -34,21 +34,49 @@ io.sockets.on('connection', function(socket) {
     function updateUsersNames() {
           io.sockets.emit('usernames', Object.keys(usersNames));
     }
-    //if user leaves chat, then username removed from UsersNames[]
+    //if user leaves chat, then username removed from UsersNames{}
     socket.on('disconnect', function(data){
-        if(!socket.userName) return;
+        if(!socket.userName) return; //if there s no username simply return
+	       // remove the username from global usernames list
         delete usersNames[socket.userName];
-        //usersNames.splice(usersNames.indexOf(socket.userName), 1);
         updateUsersNames();
     });
+
+
+    // when the user sends a private message to a user.. perform this
+  	socket.on('send message', function(data) {
+  		console.log("From user: "+socket.userName);
+  		console.log("To user: "+data.client);
+  		console.log(usersNames);
+      socket.to(usersNames[data.client]).emit('new message', //username, msg);
+          {
+            'sender': socket.userName,
+            'message': data.msg,
+            'client': data.client
+          });
+      });
+
+
+
+
+
+
+/*
     //user sends message from clientside
     socket.on('send message', function(data){
-        io.sockets.emit('new message', {          //server sends msg to all users including me
-          name: socket.userName,
-          message: data
+      console.log(data.client);//prints message receiver's name
+      console.log(usersNames);
+      var receiver = data.client;
+      console.log(receiver);
+      io.sockets.connected[receiver].emit('new message', {
+        //  io.sockets.emit('new message', {
+          sender: socket.userName,
+          message: data.msg,
+          client: data.client
         });
-
     });
+*/
+
 
     socket.on('typing message', function(data){
         socket.broadcast.emit('typing message', socket.userName); //server sends msg to all excluding me
